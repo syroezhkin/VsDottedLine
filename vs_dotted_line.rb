f9bc13581cf36bbbaeb00ead605e38a4ee55518d
Vladimir Syroezhkin <public.syroezhkin@me.com>
2015-11-09 13:22:39 +0200

    Coding style was corrected

--- vs_dotted_line.rb
+++ vs_dotted_line.rb
@@ -5,76 +5,79 @@ require 'sketchup.rb'
 
 module VsDottedLine
 
-    def self.selected_edges # checks whether the selection is the edge and returns its entity
-        model = Sketchup.active_model
-        entities = model.active_entities
-        selection = model.selection
-        return nil if selection.empty?
-        selection.each do |sel|
-            return nil if not (sel.instance_of? Sketchup::Edge)
-        end
-        selection
+  # Checking whether the selection is the edge and returns its entity
+  def self.selected_edges
+    model = Sketchup.active_model
+    entities = model.active_entities
+    selection = model.selection
+    return nil if selection.empty?
+    selection.each { |sel| return nil unless (sel.instance_of? Sketchup::Edge) }
+    selection
+  end
+
+  # Accepts coordinates of two points with the properties and creates the dotted line
+  def self.add_dotted_line(start_point, end_point, dot, space)
+    step = dot + space
+    distance = start_point.distance end_point
+    number_of_lines = (distance / step).to_i
+
+    vector = start_point.vector_to end_point
+    vector_dot = vector.clone
+    vector_dot.length = dot
+    vector_step = vector.clone
+    vector_step.length = step
+
+    model = Sketchup.active_model
+    entities = model.active_entities
+    pt1 = start_point
+    pt2 = start_point.offset vector_dot
+
+    number_of_lines.times do
+      line = entities.add_line pt1, pt2
+      pt1 = pt1.offset! vector_step
+      pt2 = pt2.offset! vector_step
     end
 
-    def self.add_dotted_line(start_point, end_point, dot, space) # accepts coordinates of two points with the properties and creates the dotted line
-        step = dot + space
-        distance = start_point.distance end_point
-        number_of_lines = (distance / step).to_i
+    last_line = entities.add_line pt1, end_point
+  end
 
-        vector = start_point.vector_to end_point
-        vector_dot = vector.clone
-        vector_dot.length = dot
-        vector_step = vector.clone
-        vector_step.length = step
+  # Accepts the selected edges and replaces it by dotted lines
+  def self.replace_by_dotted_lines(selection)
+    prompts = ["Dots", "Spaces"]
+    defaults = [100.0.mm, 20.0.mm]
+    input = UI.inputbox(prompts, defaults, "Enter the line properties")
+    dot = input[0]
+    space = input[1]
 
-        model = Sketchup.active_model
-        entities = model.active_entities
-        pt1 = start_point
-        pt2 = start_point.offset vector_dot
+    model = Sketchup.active_model
+    entities = model.active_entities
+    model.start_operation "Replacing The Lines"
 
-        number_of_lines.times do
-            line = entities.add_line pt1, pt2
-            pt1 = pt1.offset! vector_step
-            pt2 = pt2.offset! vector_step
-        end
+    selection_count = VsDottedLine.selected_edges.count
 
-        last_line = entities.add_line pt1, end_point
+    selection_count.times do
+      selection.each do |edge|
+        pt1 = edge.start.position
+        pt2 = edge.end.position
+        entities.erase_entities edge
+        dotted_line = VsDottedLine.add_dotted_line(pt1, pt2, dot, space)
+      end
     end
 
-    def self.replace_by_dotted_lines(selection) # accepts the selected edges and replaces it by dotted lines
-        prompts = ["Dots", "Spaces"]
-        defaults = [100.0.mm, 20.0.mm]
-        input = UI.inputbox(prompts, defaults, "Enter the line properties")
-        dot = input[0]
-        space = input[1]
+    model.commit_operation # Replacing The Lines
+  end
 
-        model = Sketchup.active_model
-        entities = model.active_entities
-        model.start_operation "Replacing The Lines"
+end # module VsDottedLine
 
-        selection_count = VsDottedLine.selected_edges.count
-
-        selection_count.times do
-            selection.each do |edge|
-                pt1 = edge.start.position
-                pt2 = edge.end.position
-                entities.erase_entities edge
-                dotted_line = VsDottedLine.add_dotted_line(pt1, pt2, dot, space)
-            end
-        end
-
-        model.commit_operation # Replacing The Lines
+# Create menu items
+unless file_loaded?("__FILE__")
+  # Add context menu
+  UI.add_context_menu_handler do |menu|
+    if(selection = VsDottedLine.selected_edges)
+      menu.add_separator
+      menu.add_item("Replace by dotted lines...") { VsDottedLine.replace_by_dotted_lines selection }
     end
-
-end # module
-
-if( not file_loaded?("vs_dotted_line.rb") )
-    UI.add_context_menu_handler do |menu|
-        if(selection = VsDottedLine.selected_edges)
-            menu.add_separator
-            menu.add_item("Replace by dotted lines...") { VsDottedLine.replace_by_dotted_lines selection }
-        end
-    end
+  end
 end
 
-file_loaded("vs_dotted_line.rb")
+file_loaded("__FILE__")
