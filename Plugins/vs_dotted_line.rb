d71b89b55a4d055a482f880d34fd6df408750e27
Vladimir Syroezhkin <public.syroezhkin@me.com>
2016-02-07 16:30:00 +0200

    Saving line properties added

--- Plugins/vs_dotted_line.rb
+++ Plugins/vs_dotted_line.rb
@@ -10,7 +10,7 @@ module VS
 
   vs_dotted_line = SketchupExtension.new("VS Dotted Line", (File.join(File.dirname(__FILE__),"vs_dotted_line","dotted_line_loader.rb")))
   vs_dotted_line.description=("Replace selected edges by dotted line.")
-  vs_dotted_line.version="1.0.0"
+  vs_dotted_line.version="1.0.1"
   vs_dotted_line.creator="Vladimir Syroezhkin"
   vs_dotted_line.copyright="© 2015—2016, Vladimir Syroezhkin"
   Sketchup.register_extension vs_dotted_line,true
--- Plugins/vs_dotted_line/inputbox.rb
+++ Plugins/vs_dotted_line/inputbox.rb
@@ -4,14 +4,22 @@
 module VS::DottedLine
 
   def self.inputbox
+    model = Sketchup.active_model
+    attr_name = 'VsDottedLine'
+    dot    = model.get_attribute attr_name, 'dots',   200.mm
+    space  = model.get_attribute attr_name, 'spaces', 50.mm
+    soften = model.get_attribute attr_name, 'soften', 'No'
     prompts = ['Dots', 'Spaces', 'Soften']
-    defaults = [200.0.mm, 50.0.mm, 'No']
+    defaults = [dot, space, soften]
     lists = ['', '', 'Yes|No']
     input = UI.inputbox(prompts, defaults, lists, 'Line properties')
     return nil if not input
     dot = input[0]
     space = input[1]
     soften = (input[2] == 'Yes') ? true : false
+    model.set_attribute attr_name, 'dots',   dot
+    model.set_attribute attr_name, 'spaces', space
+    model.set_attribute attr_name, 'soften', soften == true ? 'Yes' : 'No'
     [dot, space, soften]
   end
 
